b83ebffc8d196b1d1ceb94cfb044d615ca6d1175
ScoobyLuffyDoo <ScoobyLuffyDoo@gmail.com>
2022-04-30 00:46:38 +0200

    Unlimited Password Length Complete

--- UsernamePassword/generator.py
+++ UsernamePassword/generator.py
@@ -6,12 +6,13 @@ character_set ='abcdefghijklmnopqrstuvwxyz'
 upper_character_set = character_set.upper()
 numbers_set = "0123456789"
 special_character='!@#$%^&*()_+=|?<>/'
-all_characters = random.shuffle(f'{character_set}{upper_character_set }{numbers_set}{special_character}')
+all_characters = character_set+upper_character_set+numbers_set+special_character
+
 class Generator():
     @staticmethod
     def get_password(length):
         # get new length
-        tempset= random.sample(all_characters,length)
+        tempset= random.choices(all_characters, k=length)
         password= "".join(tempset)
         return(password)
     
